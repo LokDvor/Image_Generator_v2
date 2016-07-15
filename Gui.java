81bc5a983d1954d6a2bf8cf44d7ede9b0a2b0246
Andrey <andreymorison@gmail.com>
2016-07-15 19:11:11 +0400

    Add files via upload

---
+++ Gui.java
@@ -0,0 +1,412 @@
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JSlider;
+import javax.swing.JTextField;
+import javax.swing.SpringLayout;
+
+public class Gui extends JFrame implements ActionListener{
+	
+	static final int COLOR_MIN = 0;
+	static final int COLOR_MAX = 8388608;
+	static final int COLOR_INIT = 0;
+
+	private JLabel size_x_label = new JLabel("Frame x:");
+	private JLabel size_y_label = new JLabel("Frame y:");
+	private JTextField size_x_field = new JTextField(10);
+	private JTextField size_y_field = new JTextField(10);
+	
+	private JLabel bend_x_label = new JLabel("Bend x:");
+	private JLabel bend_y_label = new JLabel("Bend y:");
+	private JTextField bend_x_field = new JTextField(10);
+	private JTextField bend_y_field = new JTextField(10);
+	
+	private JLabel distance_label = new JLabel("Distance:");
+	private JTextField distance_field = new JTextField(10);
+	
+	private JLabel speed_label = new JLabel("Speed:");
+	private JTextField speed_field = new JTextField(10);
+	
+	private JLabel num_label = new JLabel("Frames:");
+	private JTextField num_field = new JTextField(10);
+	
+	private JLabel fps_label = new JLabel("FPS:");
+	private JTextField fps_field = new JTextField(10);
+	
+	JSlider bend_slider = new JSlider(JSlider.HORIZONTAL, COLOR_MIN, COLOR_MAX, COLOR_INIT);
+	JSlider back_slider = new JSlider(JSlider.HORIZONTAL, COLOR_MIN, COLOR_MAX, COLOR_INIT);
+	
+	private JLabel bend_color_label = new JLabel("Bend Color:");
+	private JLabel back_color_label = new JLabel("Back Color:");
+	
+	private SpringLayout layout = new SpringLayout();
+	
+	private JButton gen = new JButton("Generate");
+	
+	public Gui(int size_x, int size_y)
+	{
+		init(size_x, size_y);
+	}
+	
+	public void init(int size_x, int size_y)
+	{
+		
+		   Container contentPane = this.getContentPane();
+        this.setLayout(layout);
+       gen.addActionListener(this);
+        
+      //==============================================================
+        
+        this.add(size_x_label);
+        this.add(size_x_field);
+        
+        layout.putConstraint(SpringLayout.WEST, size_x_label,
+                5,
+                SpringLayout.WEST, contentPane);
+        layout.putConstraint(SpringLayout.NORTH, size_x_label,
+                5,
+                SpringLayout.NORTH, contentPane);
+
+
+	layout.putConstraint(SpringLayout.WEST, size_x_field,
+                5,
+                SpringLayout.EAST, size_x_label);
+	layout.putConstraint(SpringLayout.NORTH, size_x_field,
+                5,
+                SpringLayout.NORTH, contentPane);
+
+
+	layout.putConstraint(SpringLayout.EAST, contentPane,
+                5,
+                SpringLayout.EAST, size_x_field);
+	layout.putConstraint(SpringLayout.SOUTH, contentPane,
+                5,
+                SpringLayout.SOUTH, size_x_field);
+
+	
+	//==============================================================
+	
+	   this.add(size_y_label);
+       this.add(size_y_field);
+       
+       layout.putConstraint(SpringLayout.WEST, size_y_label,
+               5,
+               SpringLayout.WEST, contentPane);
+       
+       layout.putConstraint(SpringLayout.NORTH, size_y_label,
+               25,
+               SpringLayout.NORTH, size_x_label);
+
+
+	layout.putConstraint(SpringLayout.WEST, size_y_field,
+               5,
+               SpringLayout.EAST, size_y_label);
+	layout.putConstraint(SpringLayout.NORTH, size_y_field,
+               25,
+               SpringLayout.NORTH, size_x_field);
+
+
+	layout.putConstraint(SpringLayout.EAST, contentPane,
+               5,
+               SpringLayout.EAST, size_y_field);
+	layout.putConstraint(SpringLayout.SOUTH, contentPane,
+               5,
+               SpringLayout.SOUTH, size_y_field);
+		
+	//==============================================================
+	
+	 this.add(bend_x_label);
+     this.add(bend_x_field);
+     
+     layout.putConstraint(SpringLayout.WEST, bend_x_label,
+             5,
+             SpringLayout.WEST, contentPane);
+     
+     layout.putConstraint(SpringLayout.NORTH, bend_x_label,
+             25,
+             SpringLayout.NORTH, size_y_label);
+
+
+	layout.putConstraint(SpringLayout.WEST, bend_x_field,
+             11,
+             SpringLayout.EAST, bend_x_label);
+	
+	layout.putConstraint(SpringLayout.NORTH, bend_x_field,
+             25,
+             SpringLayout.NORTH, size_y_field);
+
+
+	layout.putConstraint(SpringLayout.EAST, contentPane,
+             5,
+             SpringLayout.EAST, bend_x_field);
+	layout.putConstraint(SpringLayout.SOUTH, contentPane,
+             5,
+             SpringLayout.SOUTH, bend_x_field);
+	
+	//==============================================================
+	
+	this.add(bend_y_label);
+    this.add(bend_y_field);
+    
+    layout.putConstraint(SpringLayout.WEST, bend_y_label,
+            5,
+            SpringLayout.WEST, contentPane);
+    
+    layout.putConstraint(SpringLayout.NORTH, bend_y_label,
+            25,
+            SpringLayout.NORTH, bend_x_label);
+
+
+	layout.putConstraint(SpringLayout.WEST, bend_y_field,
+            11,
+            SpringLayout.EAST, bend_y_label);
+	
+	layout.putConstraint(SpringLayout.NORTH, bend_y_field,
+            25,
+            SpringLayout.NORTH, bend_x_field);
+
+
+	layout.putConstraint(SpringLayout.EAST, contentPane,
+            5,
+            SpringLayout.EAST, bend_y_field);
+	layout.putConstraint(SpringLayout.SOUTH, contentPane,
+            5,
+            SpringLayout.SOUTH, bend_y_field);
+	
+	//==============================================================
+	
+	this.add(distance_field);
+	this.add(distance_label);
+	
+    layout.putConstraint(SpringLayout.WEST, distance_label,
+            5,
+            SpringLayout.WEST, contentPane);
+    
+    layout.putConstraint(SpringLayout.NORTH, distance_label,
+            25,
+            SpringLayout.NORTH, bend_y_label);
+
+
+	layout.putConstraint(SpringLayout.WEST, distance_field,
+            1,
+            SpringLayout.EAST, distance_label);
+	
+	layout.putConstraint(SpringLayout.NORTH, distance_field,
+            25,
+            SpringLayout.NORTH, bend_y_field);
+
+
+	layout.putConstraint(SpringLayout.EAST, contentPane,
+            5,
+            SpringLayout.EAST, distance_field);
+	layout.putConstraint(SpringLayout.SOUTH, contentPane,
+            5,
+            SpringLayout.SOUTH, distance_field);
+	
+	//==============================================================
+	
+		this.add(speed_field);
+		this.add(speed_label);
+		
+	    layout.putConstraint(SpringLayout.WEST, speed_label,
+	            5,
+	            SpringLayout.WEST, contentPane);
+	    
+	    layout.putConstraint(SpringLayout.NORTH, speed_label,
+	            25,
+	            SpringLayout.NORTH, distance_label);
+
+
+		layout.putConstraint(SpringLayout.WEST, speed_field,
+	            15,
+	            SpringLayout.EAST, speed_label);
+		
+		layout.putConstraint(SpringLayout.NORTH, speed_field,
+	            25,
+	            SpringLayout.NORTH, distance_field);
+
+
+		layout.putConstraint(SpringLayout.EAST, contentPane,
+	            5,
+	            SpringLayout.EAST, speed_field);
+		layout.putConstraint(SpringLayout.SOUTH, contentPane,
+	            5,
+	            SpringLayout.SOUTH, speed_field);	
+		//==============================================================
+		
+			this.add(num_field);
+			this.add(num_label);
+			
+		    layout.putConstraint(SpringLayout.WEST, num_label,
+		            5,
+		            SpringLayout.WEST, contentPane);
+		    
+		    layout.putConstraint(SpringLayout.NORTH, num_label,
+		            25,
+		            SpringLayout.NORTH, speed_label);
+
+
+			layout.putConstraint(SpringLayout.WEST, num_field,
+		            15,
+		            SpringLayout.EAST, speed_label);
+			
+			layout.putConstraint(SpringLayout.NORTH, num_field,
+		            25,
+		            SpringLayout.NORTH, speed_field);
+
+
+			layout.putConstraint(SpringLayout.EAST, contentPane,
+		            5,
+		            SpringLayout.EAST, num_field);
+			layout.putConstraint(SpringLayout.SOUTH, contentPane,
+		            5,
+		            SpringLayout.SOUTH, num_field);	
+	//===============================================================
+			
+			
+			this.add(fps_field);
+			this.add(fps_label);
+			
+		    layout.putConstraint(SpringLayout.WEST, fps_label,
+		            5,
+		            SpringLayout.WEST, contentPane);
+		    
+		    layout.putConstraint(SpringLayout.NORTH, fps_label,
+		            25,
+		            SpringLayout.NORTH, num_label);
+
+
+			layout.putConstraint(SpringLayout.WEST, fps_field,
+		            15,
+		            SpringLayout.EAST, fps_label);
+			
+			layout.putConstraint(SpringLayout.NORTH, fps_field,
+		            25,
+		            SpringLayout.NORTH, num_field);
+
+
+			layout.putConstraint(SpringLayout.EAST, contentPane,
+		            5,
+		            SpringLayout.EAST, fps_field);
+			layout.putConstraint(SpringLayout.SOUTH, contentPane,
+		            5,
+		            SpringLayout.SOUTH, fps_field);	
+	//===============================================================
+			
+	
+			this.add(bend_slider);
+			this.add(bend_color_label);
+			
+		    layout.putConstraint(SpringLayout.WEST, bend_color_label,
+		            5,
+		            SpringLayout.WEST, contentPane);
+		    
+		    layout.putConstraint(SpringLayout.NORTH, bend_color_label,
+		            25,
+		            SpringLayout.NORTH, fps_label);
+
+
+			layout.putConstraint(SpringLayout.WEST, bend_slider,
+		            15,
+		            SpringLayout.EAST, bend_color_label);
+			
+			layout.putConstraint(SpringLayout.NORTH, bend_slider,
+		            25,
+		            SpringLayout.NORTH, fps_field);
+
+
+			layout.putConstraint(SpringLayout.EAST, contentPane,
+		            5,
+		            SpringLayout.EAST, bend_slider);
+			layout.putConstraint(SpringLayout.SOUTH, contentPane,
+		            5,
+		            SpringLayout.SOUTH, bend_slider);	
+	//===============================================================
+			
+			
+			this.add(back_slider);
+			this.add(back_color_label);
+			
+		    layout.putConstraint(SpringLayout.WEST, back_color_label,
+		            5,
+		            SpringLayout.WEST, contentPane);
+		    
+		    layout.putConstraint(SpringLayout.NORTH, back_color_label,
+		            25,
+		            SpringLayout.NORTH, bend_color_label);
+
+
+			layout.putConstraint(SpringLayout.WEST, back_slider,
+		            15,
+		            SpringLayout.EAST, back_color_label);
+			
+			layout.putConstraint(SpringLayout.NORTH, back_slider,
+		            25,
+		            SpringLayout.NORTH, bend_slider);
+
+
+			layout.putConstraint(SpringLayout.EAST, contentPane,
+		            5,
+		            SpringLayout.EAST, back_slider);
+			layout.putConstraint(SpringLayout.SOUTH, contentPane,
+		            5,
+		            SpringLayout.SOUTH, back_slider);	
+	//===============================================================
+			
+			
+			this.add(gen);	
+			
+			layout.putConstraint(SpringLayout.WEST, gen,
+		            5,
+		            SpringLayout.WEST, contentPane);
+		    
+		    layout.putConstraint(SpringLayout.NORTH, gen,
+		            25,
+		            SpringLayout.NORTH, back_color_label);
+
+			layout.putConstraint(SpringLayout.SOUTH, contentPane,
+		            5,
+		            SpringLayout.SOUTH, gen);	
+			
+			
+			
+		this.setVisible(true);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);	
+		
+		this.pack();
+		
+	}
+	
+	
+	@Override
+	public void actionPerformed(ActionEvent arg0)
+	{
+		if (arg0.getSource().equals(gen))
+		{
+		
+			String[] args = new String[10];
+			
+			
+			args[0] = size_x_field.getText();
+			args[1] = size_y_field.getText();
+			args[2] = bend_x_field.getText();
+			args[3] = bend_y_field.getText();
+			args[4] = distance_field.getText();
+			args[5] = speed_field.getText();
+			args[6] = num_field.getText();
+			args[7] = fps_field.getText();
+			args[8] = Integer.toString(bend_slider.getValue());
+			args[9] = Integer.toString(back_slider.getValue());
+			
+			Window window = new Window(args);
+		}
+	}
+	
+}
---
+++ Window.java
@@ -0,0 +1,259 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class Window extends JFrame implements Runnable
+{
+	private JPanel render_panel = new JPanel();
+	private BufferedImage image;
+	private Color bend_color = Color.RED;
+	private Color back_color = Color.BLUE;
+	private Graphics g;
+	private int size_x = 600, size_y = 600 ;
+	private int frames = 1;
+	private int speed = 4;
+	private boolean error = false; 
+	private int bend_width = 100;
+	private int bend_height = 200;
+	private int distance;
+	private int y_cord = size_y/2;
+	private int num = 10;
+	private int tick;
+	private int image_num = 0;
+	private boolean isRunning;
+	
+	
+	public Window(String[] args)
+	{
+		args_parser(args);
+		init();
+	}
+	
+	public void args_parser(String[] args)
+	{
+		size_x = Integer.parseInt(args[0]);
+		size_y = Integer.parseInt(args[1]);
+		
+		if (size_x > 0 && size_y > 0  && size_x < 3000 && size_y < 3000)
+		{
+			System.out.println("Frame width = " + args[0]);
+		
+			System.out.println("Frame height = " + args[1]);
+		}
+		else 
+		{
+			System.out.println("Frame values out of range (0 - 3000)");
+			System.exit(-1);
+		}
+		
+		bend_width = Integer.parseInt(args[2]);
+		bend_height = Integer.parseInt(args[3]);
+		
+		if (bend_width> 0 && bend_height > 0  && bend_width < size_x && bend_height < size_y)
+		{
+			System.out.println("Bend width = " + bend_width);
+		
+			System.out.println("Frame height = " + bend_height);
+		}
+		else 
+		{
+			System.out.println("Bend values out of range (0 - Frame size)");
+			error = true;
+			 on_close();
+		}
+		
+		distance = Integer.parseInt(args[4]);
+		
+		if (distance > size_y && distance < 5000)
+		{
+			System.out.println("Distance = " + distance);
+		}
+		else 
+		{
+			System.out.println("Distance values out of range (Frame_y - 5000)");
+			error = true;
+			 on_close();
+		}
+		
+		speed = Integer.parseInt(args[5]);
+		
+		if (speed < 50 && speed > -50)
+		{
+			System.out.println("Speed = " + speed);
+		}
+		else 
+		{
+			System.out.println("Speed values out of range (- 50 - 50)");
+			error = true;
+			 on_close();
+		}
+		
+		num = Integer.parseInt(args[6]);
+		
+		if (num > 0 && num < 101)
+		{
+			System.out.println("Number of frames = " + num);
+		}
+		else 
+		{
+			System.out.println("Num of Frames values out of range (0 - 100)");
+			error = true;
+			 on_close();
+		}
+		
+		frames = Integer.parseInt(args[7]);
+		
+		if (frames > 0 && frames < 100)
+		{
+			System.out.println("FPS = " + num);
+		}
+		else 
+		{
+			System.out.println("FPS values out of range (1 - 100)");
+			error = true;
+			 on_close();
+		}
+		
+		bend_color = new Color(Integer.parseInt(args[8]));
+		back_color = new Color(Integer.parseInt(args[9]));
+		
+	}
+	
+	
+	public void init()
+	{
+		
+		WindowListener exitListener = new WindowAdapter() {
+
+		    @Override
+		    public void windowClosing(WindowEvent e) {
+		       on_close();
+		        }
+		    
+		};
+		
+		this.addWindowListener(exitListener);
+		this.setSize(size_x, size_y);
+		this.setResizable(false);
+		this.add(render_panel);
+		this.setVisible(true);
+		g = render_panel.getGraphics();
+		
+		render(g);
+		isRunning = true;
+		new Thread(this).start();
+	}
+	
+	public int on_close()
+	{
+		isRunning = false;
+		System.out.println("FINISHED");
+		return 1;
+	}
+	
+	public void render(Graphics g)
+	{
+		image = new BufferedImage(size_x, size_y,  BufferedImage.TYPE_INT_ARGB);
+		Graphics g2 = image.createGraphics();
+		
+		g2.setColor(back_color);				
+		g2.fillRect(0, 0, size_x, size_y);
+		
+		g2.setColor(bend_color);	
+		g2.fillRect( (int)(size_x/2 - bend_width/2) , y_cord, bend_width, bend_height);
+			
+		
+		g.drawImage(image, 0, 0, null);
+		
+		
+		
+	}
+	
+	
+	
+	public static void main(String[] args)
+	{
+		
+		Gui gui = new Gui(350, 600);
+	}
+
+
+	@Override
+	public void run() 
+	{
+		
+		while (isRunning && !error)
+		{
+			try {
+				Thread.sleep(40);
+			} catch (InterruptedException e) {
+			
+				e.printStackTrace();
+			}
+			
+			render(g);
+			
+			if (y_cord < -1*bend_height)
+			{
+				y_cord = distance;
+			}
+			
+			if (y_cord >  distance)
+			{
+				y_cord =  - 1*bend_height;
+			}
+			
+			y_cord = y_cord + speed;
+			tick++;
+			
+			
+			if ((tick == frames) && (num != 0))
+			{
+			try 
+			{  
+				File outputFile = new File("./Stream/FRAME_" + image_num + ".png");
+				outputFile.getParentFile().mkdirs();
+				ImageIO.write(image, "png", outputFile);
+				image_num++;
+				num--;
+				tick = 0;
+			}
+		
+				
+			catch (IOException e) 	
+			{
+			e.printStackTrace();
+			}
+			
+			if (num == 0)
+			{
+	
+				isRunning = false;
+				g.dispose();
+				System.out.println("Finished: Success!!");
+				this.dispose();
+			}
+		}
+			
+	}
+		this.dispose();
+	}
+}
